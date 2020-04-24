87b67655c2f3a25be14081d8b6a259a7a8075295
Philipp <philipp.gruber02@gmail.com>
2020-04-25 00:45:00 +0200

    olleszommgepext

---
+++ src/sample/Computer.java
@@ -0,0 +1,40 @@
+package it.bx.fallmerayer.tfo.rein.in.die.masse;
+
+import javafx.scene.image.ImageView;
+
+public class Computer {
+        private String ip;
+        private String mac;
+        private ImageView imageView;
+
+        public Computer(String ip, String mac, ImageView imageView) {
+            this.ip = ip;
+            this.mac = mac;
+            this.imageView = imageView;
+        }
+
+        public String getIp() {
+            return ip;
+        }
+
+        public void setIp(String ip) {
+            this.ip = ip;
+        }
+
+        public String getMac() {
+            return mac;
+        }
+
+        public void setMac(String mac) {
+            this.mac = mac;
+        }
+
+        public ImageView getImageView() {
+            return imageView;
+        }
+
+        public void setImageView(ImageView imageView) {
+            this.imageView = imageView;
+        }
+
+}
