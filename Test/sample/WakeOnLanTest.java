3ff0c07dadccbd0b3ba80c11d12154e20292958b
Silas <silaskeim02@gmail.com>
2020-05-06 15:14:30 +0200

    wake on lan test

--- Test/sample/WakeOnLanTest.java
+++ Test/sample/WakeOnLanTest.java
@@ -1,9 +1,42 @@
 package sample;
 
+import org.junit.jupiter.api.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class WakeOnLanTest {
 
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    void correctPC(){
+        Computer computer = new Computer("10.10.30.212","9C-7B-EF-AC-8E-14");
+        WakeOnLan wakeOnLan = new WakeOnLan(computer);
+        wakeOnLan.wakeOnLan(computer);
+        assertEquals("Wake-on-LAN Paket gesendet.", outContent.toString());
+    }
+
+    @Test
+    void wrongPC(){
+        Computer computer = new Computer("10.10.30.201","98-7B-EF-AC-8E-14");
+        WakeOnLan wakeOnLan = new WakeOnLan(computer);
+        wakeOnLan.wakeOnLan(computer);
+        assertEquals("Wake-on-LAN Paket nicht gesendet!", outContent.toString());
+    }
 
 
 }
