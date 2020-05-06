111c17a53e7b78fa6685dde15f4d1946460e158d
Therealdanjo <Therealdanjo@users.noreply.github.com>
2020-05-06 15:31:35 +0200

    Removed unnecessary test

--- Test/sample/PingComputerTest.java
+++ Test/sample/PingComputerTest.java
@@ -6,6 +6,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PingComputerTest {
 
+    /**
+     * Pings a pc with fake ip-Address
+     */
     @Test
     void fakeIP(){
         Computer computer = new Computer("10.10.30.200","9C-7B-EF-AC-8E-14");
@@ -14,19 +17,16 @@ class PingComputerTest {
         assertFalse(erg);
     }
 
-    @Test
-    void fakeMAC(){
-        Computer computer = new Computer("10.10.30.201","98-7B-EF-AC-8E-14");
-        PingComputer ping = new PingComputer(computer);
-        boolean erg = ping.pingComputer(computer);
-        assertFalse(erg);
-    }
-
+    /**
+     * Pings a online pc with correct ip-Address
+     */
     @Test
     void correctPC(){
-        Computer computer = new Computer("10.10.30.212","9C-7B-EF-AC-8E-14");
+        Computer computer = new Computer("10.10.30.212","9C-7B-EF-AB-FE-BA");
         PingComputer ping = new PingComputer(computer);
         boolean erg = ping.pingComputer(computer);
         assertTrue(erg);
     }
+
+
 }
